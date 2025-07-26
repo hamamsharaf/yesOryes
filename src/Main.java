2b7eea8d833711d0acbd8acc29882bdb269f40dc
hamamfharaf <hamamfharaf06@gmail.com>
2025-07-26 21:48:44 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = in.nextLine().toLowerCase();
+            if (s.equals("yes")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
+
